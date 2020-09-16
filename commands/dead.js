e31908cbd576a9d195d766119dbbebc28b48247d
Jason Diaz Jimenez <Yadiel.diazjimenez@outlook.com>
2020-09-16 11:59:14 -0500

    Update dead.js

--- commands/dead.js
+++ commands/dead.js
@@ -6,7 +6,7 @@ exports.run = async (client, message, args) => {
         .setTitle("Chat!")
         .setDescription("Always pointing out that a chat is dead doesn't get you a reward or anything. In fact it doesn't help the server in any way. So instead of saying how inactive a channel is, try to get things moving again! Anything is better than complaining about it :3")
     .setColor("#8800FF")
-        .setFooter("Credits to https://dontasktoask.com/")
+        .setFooter("Credits to https://dead-ch.at/")
     await message.channel.send(embed)
 };
 
