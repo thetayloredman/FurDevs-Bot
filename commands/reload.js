7172226359d213c5dfb2f4f81197336d5ebd764e
Jason Diaz Jimenez <Yadiel.diazjimenez@outlook.com>
2020-09-22 09:28:57 -0500

    aaaaaa

---
+++ commands/reload.js
@@ -0,0 +1,22 @@
+const { MessageEmbed } = require("discord.js")
+const { checkBotOwner } = require("./../utils/permissions")
+const { reload } = require("./../utils/utils")
+
+exports.run = async (client, message) => {
+    await message.delete();
+    await checkBotOwner(message)
+    const cmdFiles = await readdirSync("./commands/");
+    console.log(`Loading a total of ${cmdFiles.length} commands.`);
+    cmdFiles.forEach(cmd => {
+        if (!cmd.endsWith(".js")) return;
+        const response = reload(client, cmd);
+        if (response) console.log(response);
+    });
+};
+
+exports.help = {
+    name: "reload",
+    description: "Reload the bot.",
+    usage: "",
+    aliases: ["rl"],
+};
--- utils/utils.js
+++ utils/utils.js
@@ -18,6 +18,24 @@ function load(client, command){
     }
 }
 
+function reload(client, command){
+    try {
+        const props = require(`../commands/${command}`);
+        if (props.init) {
+            props.init(client);
+        }
+        client.commands.remove(props.help.name, props);
+        client.commands.set(props.help.name, props);
+        props.help.aliases.forEach(alias => {
+          client.aliases.remove(alias, props.help.name);
+            client.aliases.set(alias, props.help.name);
+        });
+        return false;
+    } catch (e) {
+        return `Error: Unable to load command ${command}: ${e}`;
+    }
+}
+
 function execute(command) {
     return new Promise((resolve) => {
       exec(command, async (err, stdout, stderr) => {
@@ -45,3 +63,4 @@ function uid(){
 exports.execute = execute;
 exports.uid = uid
 exports.load = load
+exports.reload = reload
