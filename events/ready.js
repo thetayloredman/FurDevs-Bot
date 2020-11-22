bf393479ed53a71d68a469e10cc9cc4f923c8ff3
Jason Diaz Jimenez <yadiel.diazjimenez@outlook.com>
2020-11-22 16:55:23 -0600

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -13,19 +13,19 @@ module.exports = async client => {
     console.log('==================================')
     console.debug(`${client.user.username} is Ready!`);
 
-    var twoHours = new CronJob('00 00 2 * * *', async function () {
+    var twoHours = new CronJob('00 00 */2 * * *', async function () {
         const guild = client.guilds.cache.get("731520035717251142")
         const setting = await guild.settings()
         const bumpingChannel = setting.bumpingChannel;
         guild.channels.cache.get(bumpingChannel).send("[ TEST ] Time to bump!")
     }, null, true, 'America/Chicago');
-    var sixHours = new CronJob('00 00 6 * * *', async function () {
+    var sixHours = new CronJob('00 00 */6 * * *', async function () {
         const guild = client.guilds.cache.get("731520035717251142")
         const setting = await guild.settings()
         const bumpingChannel = setting.bumpingChannel;
         guild.channels.cache.get(bumpingChannel).send("[ TEST ] Time to bump!")
     }, null, true, 'America/Chicago');
-    var twelveHours = new CronJob('00 00 12 * * *', async function () {
+    var twelveHours = new CronJob('00 00 */12 * * *', async function () {
         const guild = client.guilds.cache.get("731520035717251142")
         const setting = await guild.settings()
         const bumpingChannel = setting.bumpingChannel;
