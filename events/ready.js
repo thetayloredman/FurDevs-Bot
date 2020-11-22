86812a8acea17adf1f278b25a7d0dfb1bce6d549
Jason Diaz Jimenez <yadiel.diazjimenez@outlook.com>
2020-11-22 16:51:56 -0600

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,60 +1,56 @@
 const settings = require("./../settings.json")
-const { CronJob } = require("cron")
+const {
+    CronJob
+} = require("cron")
 
 module.exports = async client => {
-        await client.user.setPresence({
-            activity: {
-                name: "My Furry Friends",
-                type: "WATCHING",
-            }
-        })
-        console.log('==================================')
-        console.debug(`${client.user.username} is Ready!`);
-        
-        var two = async () => {
-            
-            const guild = client.guilds.cache.get("778333835967594517")
-            const setting = await guild.settings() 
-            const bumpingChannel = setting.bumpingChannel;
-            guild.channels.cache.get(bumpingChannel).send("Time to bump!")
+    await client.user.setPresence({
+        activity: {
+            name: "My Furry Friends",
+            type: "WATCHING",
         }
+    })
+    console.log('==================================')
+    console.debug(`${client.user.username} is Ready!`);
 
-        var six = async () => {
-            
-            const guild = client.guilds.cache.get("778333835967594517")
-            const setting = await guild.settings() 
-            const bumpingChannel = setting.bumpingChannel;
-            guild.channels.cache.get(bumpingChannel).send("Time to bump!")
-        }
+    var twoHours = new CronJob('00 00 2 * * *', async function () {
+        const guild = client.guilds.cache.get("731520035717251142")
+        const setting = await guild.settings()
+        const bumpingChannel = setting.bumpingChannel;
+        guild.channels.cache.get(bumpingChannel).send("[ TEST ] Time to bump!")
+    }, null, true, 'America/Chicago');
+    var sixHours = new CronJob('00 00 6 * * *', async function () {
+        const guild = client.guilds.cache.get("731520035717251142")
+        const setting = await guild.settings()
+        const bumpingChannel = setting.bumpingChannel;
+        guild.channels.cache.get(bumpingChannel).send("[ TEST ] Time to bump!")
+    }, null, true, 'America/Chicago');
+    var twelveHours = new CronJob('00 00 12 * * *', async function () {
+        const guild = client.guilds.cache.get("731520035717251142")
+        const setting = await guild.settings()
+        const bumpingChannel = setting.bumpingChannel;
+        guild.channels.cache.get(bumpingChannel).send("[ TEST ] Time to bump!")
+    }, null, true, 'America/Chicago');
 
-        var twelve = async () => {
-            
-            const guild = client.guilds.cache.get("778333835967594517")
-            const setting = await guild.settings() 
-            const bumpingChannel = setting.bumpingChannel;
-            guild.channels.cache.get(bumpingChannel).send("Time to bump!")
-        }
+    var discordme = new CronJob('55 40 5,11,17,23 * * *', async function () {
+        const guild = client.guilds.cache.get("731520035717251142")
+        const setting = await guild.settings()
+        const bumpingChannel = setting.bumpingChannel;
+        guild.channels.cache.get(bumpingChannel).send("[ TEST ] Time to bump!")
+    }, null, true, 'America/Chicago');
 
-        var discordme = async () => {
-            
-            const guild = client.guilds.cache.get("778333835967594517")
-            const setting = await guild.settings() 
-            const bumpingChannel = setting.bumpingChannel;
-            guild.channels.cache.get(bumpingChannel).send("Time to bump!")
-        }
-        
-        var dme = new CronJob('0 55 5,11,17,23 * * * ', discordme, null, true, 'America/Chicago');
-        
-        var twos = new CronJob('0 55 0/2 * * * ', two, null, true, 'America/Chicago');
-        
-        var sixs = new CronJob('0 55 0/2 * * * ', six, null, true, 'America/Chicago');
-        
-        var twelves = new CronJob('0 55 0/2 * * *', twelve, null, true, 'America/Chicago');
-        
-        var twelves = new CronJob('0 55 0/2 * * *', twelve, null, true, 'America/Chicago');
-        
-        dme.start()
-        twos.start()
-        sixs.start()
-        twelves.start()
-    }
+    
+    twoHours.start()
+    discordme.start();
+    twelveHours.start();
+    sixHours.start()
+    console.log("Bump Reminders Started!")
+
+   
+    
+
+
+
+
+
+}
