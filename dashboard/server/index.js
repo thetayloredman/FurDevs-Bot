1835125ac50979921efbe10918fe67ce25bb0e8f
Jason Diaz Jimenez <Yadiel.diazjimenez@outlook.com>
2020-12-18 23:38:25 -0600

    d

--- dashboard/server/index.js
+++ dashboard/server/index.js
@@ -1,17 +1,39 @@
+require("./strategies/discord")
+const express = require("express");
+const passport = require("passport")
+const mongoose = require("mongoose")
+const session = require("express-session")
+const Store = require("connect-mongo")(session)
+const cors = require("cors")
+const app = express()
 const domain = process.env.DOMAIN
 const port = process.env.PORT
+const routes = require("./routes")
 
-const express = require('express')
-const app = express()
 
-const dashboard = (client) => {   
-    app.get('/', (req, res) => {
-        res.status(200).send("Hello World!")
+module.exports = async(client) => {
+
+    mongoose.connect(`${process.env.DB}`, {
+        useNewUrlParser:true, 
+        useUnifiedTopology: true
     })
-
+    app.use(cors({
+        origin: ["http://localhost:3000"],
+        credentials: true,
+    }))
+    app.use(session({
+        secret: process.env.CLIENTSECRET,
+        cookie:{
+            maxAge: 60000 * 60 * 24
+        },
+        resave: false,
+        saveUninitialized: false,
+        store: new Store({ mongooseConnection: mongoose.connection })
+    }))
+    app.use(passport.initialize())
+    app.use(passport.session())
+    app.use("/api", routes)
     app.listen(port, () => {
-        console.log(`${client.fwebsLog} Listening on http://${domain}:${port}`)
+        console.log(`${client.fwebsLog} Dashboard is Ready to Serve on http://${domain}:${port} !`)
     })
 }
-
-module.exports = dashboard
