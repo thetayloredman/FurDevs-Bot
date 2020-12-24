985499cc70ee8b37bace40d886dddec8b07dd746
Jason Diaz Jimenez <thejdragon16@Outlook.com>
2020-12-23 20:24:02 -0600

    Moderation Commands

--- structures/Guild.js
+++ structures/Guild.js
@@ -1,5 +1,8 @@
-const { Structures } = require("discord.js")
+const { Structures } = require('discord.js')
+const AntiRaidConfig = require("./../database/models/AntiRaidConfig");
+const AntiSpamConfig = require("./../database/models/AntiSpamConfig");
 const GuildsConfig = require("./../database/models/GuildConfig");
+const ModerationConfig = require("./../database/models/ModerationConfig");
 
 // Guilds
 Structures.extend("Guild", (Guild) => {
@@ -20,7 +23,41 @@ Structures.extend("Guild", (Guild) => {
                         }
                         console.log("Guild Settings have been created")
                     })
-                    
+                   
+                }
+
+            })
+
+            AntiRaidConfig.findOne({
+                guildID: this.id
+            }, (err, res) => {
+                if (err) return err;
+                if (!res) {
+                    const newGuild = new AntiRaidConfig({
+                        guildID: this.id
+                    })
+                    newGuild.save(function (err) {
+                        if (err) {
+                            console.log(err)
+                        }
+                    })
+
+                }
+            })
+
+            AntiSpamConfig.findOne({
+                guildID: this.id
+            }, (err, res) => {
+                if (err) return err;
+                if (!res) {
+                    const newGuild = new AntiSpamConfig({
+                        guildID: this.id
+                    })
+                    newGuild.save(function (err) {
+                        if (err) {
+                            console.log(err)
+                        }
+                    })
 
                 }
             })
@@ -28,13 +65,76 @@ Structures.extend("Guild", (Guild) => {
 
         // per-guild settings
         async settings() {
-            const data = await GuildsConfig.findOne({
-                guildID: this.id,
-            })
-            if (data) {
-                return data
-            } else {
-                throw new Error("Uh cheif, We have a problem")
+            try {
+                const data = await GuildsConfig.findOne({
+                    guildID: this.id,
+                })
+                if (data) {
+                    return data
+                } else {
+                    throw new Error("Uh cheif, We have a problem")
+                }
+            } catch (err) {
+                throw new Error("Please take a Screenshot of this embed and send it to the Support Server\n\`\`\` " + err + "\`\`\`")
+            }
+        }
+
+        // Antiraid system
+        async antiraid() {
+            try {
+                const data = await AntiRaidConfig.findOne({
+                    guildID: this.id,
+                })
+                if (data) {
+                    return data
+                }
+
+            } catch (err) {
+                throw new Error("Please take a Screenshot of this embed and send it to the Support Server\n\`\`\` " + err + "\`\`\`")
+            }
+        }
+
+        // Antispam system
+        async antispam() {
+            try {
+                const data = await AntiSpamConfig.findOne({
+                    guildID: this.id,
+                })
+                if (data) {
+                    return data
+                }
+
+            } catch (err) {
+                throw new Error("Please take a Screenshot of this embed and send it to the Support Server\n\`\`\` " + err + "\`\`\`")
+            }
+        }
+
+        // Per-guild rules
+        async rules() {
+            try {
+                const data = await RulesConfig.findOne({
+                    guildID: this.id,
+                })
+                if (data) {
+                    return data
+                }
+            } catch (err) {
+                throw new Error("Please take a Screenshot of this embed and send it to the Support Server\n\`\`\` " + err + "\`\`\`")
+            }
+        }
+
+        // Guild moderation logs
+        async moderation() {
+            try {
+                const data = await ModerationConfig.find({
+                    guildID: this.id,
+                })
+                if (data) {
+                    return data
+                }
+
+            } catch (err) {
+                throw new Error("Please take a Screenshot of this embed and send it to the Support Server\n\`\`\` " + err + "\`\`\`")
             }
         }
     }
--- structures/GuildMember.js
+++ structures/GuildMember.js
@@ -1,37 +1,58 @@
-const { Structures } = require("discord.js")
+const { Structures } = require('discord.js')
+const ModerationConfig = require("./../database/models/ModerationConfig");
 const MembersConfig = require("./../database/models/MembersConfig");
 
-// Per-Member Data
+
+
+// Guild members
 Structures.extend("GuildMember", (GuildMember) => {
     class CoolGuildMember extends GuildMember {
-      constructor(client, data, guild) {
-        super(client, data, guild);
+        constructor(client, data, guild) {
+            super(client, data, guild);
 
-        // Initialize the guild members
-        MembersConfig.findOne({ guildID: this.guild.id, userID: this.user.id }, (err, res) => {
-            if(err) return err;
-            if(!res){
-                const newGuild = new MembersConfig({ guildID: this.guild.id, userID: this.user.id})
-                newGuild.save(function(err){
-                    if(err){
-                        console.log(err)
-                    }
-                })
-                console.log("Created Database Data for a new Server that invited the bot while it was offline")
-            }
-        })
+            // Initialize the guild members
+            MembersConfig.findOne({
+                guildID: this.guild.id,
+                userID: this.id
+            }, (err, res) => {
+                if (err) return err;
+                if (!res) {
+                    const newGuild = new MembersConfig({
+                        guildID: this.guild.id,
+                        userID: this.id
+                    })
+                    newGuild.save(function (err) {
+                        if (err) {
+                            console.log(err)
+                        }
+                    })
 
-      }
-
-      // Per-member settings
-      async settings() {
-        const data = await MembersConfig.findOne({ guildID: this.guild.id, userID: this.user.id })
-        if(data){
-            return data
+                }
+            })
         }
-      }
 
+        // Per-member settings
+        async settings() {
+            const data = await MembersConfig.findOne({
+                guildID: this.guild.id,
+                userID: this.id
+            })
+            if (data) {
+                return data
+            }
+        }
+
+        // Member moderation logs
+        async moderation() {
+            const data = await ModerationConfig.find({
+                guildID: this.guild.id,
+                userID: this.id
+            })
+            if (data) {
+                return data
+            }
+        }
     }
 
     return CoolGuildMember;
-  });
+});
