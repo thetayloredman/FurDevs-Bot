7808bdfd11ac2720fba160432851c4e8e3c71fe7
Jason Diaz Jimenez <developervulpo@outlook.com>
2021-01-10 19:28:49 -0600

    Fixed Reputation Bug

--- events/message.js
+++ events/message.js
@@ -23,12 +23,16 @@ module.exports = async (client, message) => {
     var commandParts;
 
     console.log(message.content)
-    if(message.content.toLowerCase().includes("thanks") && message.mentions.members){
+    if(message.content.toLowerCase().includes("thanks") && message.mentions.members.first()){
         let msg = await message.channel.send("Would you like to give a rep to this user")
         await msg.react("✅")
         await msg.react("❌")
-        const filter = (reaction, user) => reaction.emoji.name === '✅' || reaction.emoji.name === '❌'  && user.id === message.author.id;
-        await msg.awaitReactions(filter, { time: 15000 }).then(async (collected) => {
+        let collected = await msg.awaitReactions((r, u) => r.emoji.name == "✅" || r.emoji.name == "❌" && u.id == message.author.id, {
+            max: 1,
+            time: 20000,
+            errors: ['time']
+        })
+        
             const reaction = collected.first()
             switch(reaction.emoji.name){
                 case '✅':
@@ -53,48 +57,48 @@ module.exports = async (client, message) => {
                 case "❌":
                     return message.channel.send("Aw qwq What a bummer")
             }
-        })
+        }
         
-    }
-
-    // If the user pings the bot the bot will respond with it's prefix
-    const mentionRegex = RegExp(`^<@!${client.user.id}>$`);
-    const mentionRegexPrefix = RegExp(`^<@!${client.user.id}> `);
-    if (message.content.match(mentionRegex)) {
-        return message.channel.send(`Heya ${message.author}! My Prefix is \`${GuildConfig.prefix}\``);
-    }
-
-    var prefix = message.content.match(mentionRegexPrefix) ? message.content.match(mentionRegexPrefix)[0] : GuildConfig.prefix;
-    // Is a command by a guild member who is not a bot? If so execute it
-    if (message.content.startsWith(prefix)) {
-        if (message.member && message.author && !message.author.bot) {
-            commandParts = message.content.slice(prefix.length).trim().split(/ +/g);
-            const command = commandParts.shift().toLowerCase();
-            const cmd = client.commands.get(command) || client.commands.get(client.aliases.get(command));
-            if (!cmd) {
-                return console.log(`Discord: Command Invalid: ${command} by ${message.author.tag}`)
-            } else {
-                try {
-                    await cmd.run(client, message, commandParts);
-                } catch (e) {
+        
+        // If the user pings the bot the bot will respond with it's prefix
+        const mentionRegex = RegExp(`^<@!${client.user.id}>$`);
+        const mentionRegexPrefix = RegExp(`^<@!${client.user.id}> `);
+        if (message.content.match(mentionRegex)) {
+            return message.channel.send(`Heya ${message.author}! My Prefix is \`${GuildConfig.prefix}\``);
+        }
+        
+        var prefix = message.content.match(mentionRegexPrefix) ? message.content.match(mentionRegexPrefix)[0] : GuildConfig.prefix;
+        // Is a command by a guild member who is not a bot? If so execute it
+        if (message.content.startsWith(prefix)) {
+            if (message.member && message.author && !message.author.bot) {
+                commandParts = message.content.slice(prefix.length).trim().split(/ +/g);
+                const command = commandParts.shift().toLowerCase();
+                const cmd = client.commands.get(command) || client.commands.get(client.aliases.get(command));
+                if (!cmd) {
+                    return console.log(`Discord: Command Invalid: ${command} by ${message.author.tag}`)
+                } else {
                     try {
-                        const errorMessage = new MessageEmbed()
+                        await cmd.run(client, message, commandParts);
+                    } catch (e) {
+                        try {
+                            const errorMessage = new MessageEmbed()
                             .setTitle("❌ An Error has Occured!")
                             .setThumbnail(
                                 `https://cdn.discordapp.com/emojis/604486986170105866.png?v=1`
-                            )
-                            .setDescription(`\`${command}\` failed to execute with this error \n\n${e.message}\n\u200b`)
-                            .setColor("#ee110f")
-                        console.log(`${client.fdevsError}: ${command} was executed but failed with an error;\n${e}`)
-                        return message.channel.send(errorMessage)
-                    } catch {
-                        console.log(`${client.fdevsError}: ${command} was executed but failed with an error;\n${e}`)
-                        return;
+                                )
+                                .setDescription(`\`${command}\` failed to execute with this error \n\n${e.message}\n\u200b`)
+                                .setColor("#ee110f")
+                                console.log(`${client.fdevsError}: ${command} was executed but failed with an error;\n${e}`)
+                                return message.channel.send(errorMessage)
+                            } catch {
+                                console.log(`${client.fdevsError}: ${command} was executed but failed with an error;\n${e}`)
+                                return;
+                            }
+                        }
                     }
+                    console.log(`Discord: Command Executed: ${command} by ${message.author.tag}`)
+                    
                 }
             }
-            console.log(`Discord: Command Executed: ${command} by ${message.author.tag}`)
-
-        }
-    }
-};
+        };
+    
