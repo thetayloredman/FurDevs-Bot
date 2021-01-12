ac943ac81e148093e0837338779a437d2ffed826
Jason Diaz Jimenez <developervulpo@outlook.com>
2021-01-12 02:22:22 -0600

    Updated Bumping reminders

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,3 @@
-const settings = require("./../settings.json");
-const fetch = require("node-fetch");
 const { MessageEmbed } = require("discord.js");
 const { CronJob } = require("cron");
 
@@ -38,12 +36,15 @@ module.exports = async (client) => {
         .addField("Server Monitoring", "`!bump`")
         .addField("DiscordServer", "`|bump`")
         .addField("DiscordList.io", "`dlm!bump`")
+        .setColor("#8800FF")
         .setFooter(
           "Thanks to Everyone Bumps the server | Once you have bumped, react with ☑ to the post so we know it's been done."
         );
-      guild.channels.cache.get(bumpingChannel).send("<@&778347045550424064>", {
-        embed: bumpEmbed,
-      });
+        try{
+          guild.channels.cache.get(bumpingChannel).send(bumpEmbed);
+        }catch{
+          console.log("Failed to send Bump message")
+        }
     },
     null,
     true,
@@ -76,49 +77,53 @@ module.exports = async (client) => {
         .addField("Server Monitoring", "`!bump`")
         .addField("DiscordServer", "`|bump`")
         .addField("DiscordList.io", "`dlm!bump`")
+        .setColor("#8800FF")
         .setFooter(
           "Thanks to Everyone Bumps the server | Once you have bumped, react with ☑ to the post so we know it's been done."
         );
-      guild.channels.cache
-        .get(bumpingChannel)
-        .send("<@&778347045550424064> | <@&778349310986289162>", {
-          embed: bumpEmbed,
-        });
+        try{
+          guild.channels.cache.get(bumpingChannel).send(bumpEmbed);
+        }catch{
+          console.log("Failed to send Bump message")
+        }
     },
     null,
     true,
     "America/Chicago"
   );
 
-  var discordme = new CronJob(
-    "40 55 5,11,17,23 * * *",
-    async function () {
-      const guild = client.guilds.cache.get("731520035717251142");
-      const setting = await guild.settings();
-      const bumpingChannel = setting.bumpingChannel;
-      const bumpEmbed = new MessageEmbed()
-        .setAuthor(
-          "Friendly Bump Reminder",
-          `${client.user.displayAvatarURL()}`
-        )
-        .setTitle("Heyo all, It's time to Bump")
-        .setURL("https://discord.me/dashboard")
-        .setFooter("Thanks for Ever Bumpe the server")
-        .setFooter(
-          "Thanks to Everyone Bumps the server | Once you have bumped, react with ☑ to the post so we know it's been done."
-        );
-      guild.channels.cache.get(bumpingChannel).send("<@&778347041365295164>", {
-        embed: bumpEmbed,
-      });
-    },
-    null,
-    true,
-    "America/Chicago"
-  );
+  // TODO: Make a Staff Only Channel for Discord.me Bumping Reminders
+  // var discordme = new CronJob(
+  //   "40 55 5,11,17,23 * * *",
+  //   async function () {
+  //     const guild = client.guilds.cache.get("731520035717251142");
+  //     const setting = await guild.settings();
+  //     const bumpingChannel = setting.bumpingChannel;
+  //     const bumpEmbed = new MessageEmbed()
+  //       .setAuthor(
+  //         "Friendly Bump Reminder",
+  //         `${client.user.displayAvatarURL()}`
+  //       )
+  //       .setTitle("Heyo all, It's time to Bump")
+  //       .setURL("https://discord.me/dashboard")
+        
+  //       .setFooter(
+  //         "Thanks to Everyone Bumps the server | Once you have bumped, react with ☑ to the post so we know it's been done."
+  //       );
+  //       try{
+  //         guild.channels.cache.get(bumpingChannel).send(bumpEmbed);
+  //       }catch{
+  //         console.log("Failed to send Bump message")
+  //       }
+  //   },
+  //   null,
+  //   true,
+  //   "America/Chicago"
+  // );
 
   twoHours.start();
   fourHours.start();
-  discordme.start();
+  // discordme.start();
   console.log(`${client.fdevsLog} Bump Reminders Started!`);
   console.log(`${client.fdevsLog} ${client.user.username} is Ready!`);
 };
