4cf4b06613b5181cc2bb0256b02256d2508bc8da
Jason Diaz Jimenez <developervulpo@outlook.com>
2021-01-12 01:45:47 -0600

    Hot Fix!

--- events/message.js
+++ events/message.js
@@ -67,36 +67,35 @@ module.exports = async (client, message) => {
         }
         
 
-    }
-
-    // Money generation
-    if(!timeoutBalance.has(message.author.id) && client.coinDropArray.length > 0) {
-        let balanceAdd = Math.floor(Math.random() * client.coinDropArray.length);
-        message.member.add(client.coinDropArray[balanceAdd]);
-        timeoutBalance.add(message.author.id);
-        setTimeout(() => timeoutBalance.delete(message.author.id), 60000);
-    }
-
-    // If the user pings the bot the bot will respond with it's prefix
-    const mentionRegex = RegExp(`^<@!${client.user.id}>$`);
-    const mentionRegexPrefix = RegExp(`^<@!${client.user.id}> `);
-    if (message.content.match(mentionRegex)) {
-        return message.channel.send(`Heya ${message.author}! My Prefix is \`${GuildConfig.prefix}\``);
-    }
-
-    var prefix = message.content.match(mentionRegexPrefix) ? message.content.match(mentionRegexPrefix)[0] : GuildConfig.prefix;
-    // Is a command by a guild member who is not a bot? If so execute it
-    if (message.content.startsWith(prefix)) {
-        if (message.member && message.author && !message.author.bot) {
-            commandParts = message.content.slice(prefix.length).trim().split(/ +/g);
-            const command = commandParts.shift().toLowerCase();
-            const cmd = client.commands.get(command) || client.commands.get(client.aliases.get(command));
-            if (!cmd) {
-                return console.log(`Discord: Command Invalid: ${command} by ${message.author.tag}`)
-            } else {
-                try {
-                    await cmd.run(client, message, commandParts);
-                } catch (e) {
+        
+        // Money generation
+        if(!timeoutBalance.has(message.author.id) && client.coinDropArray.length > 0) {
+            let balanceAdd = Math.floor(Math.random() * client.coinDropArray.length);
+            message.member.add(client.coinDropArray[balanceAdd]);
+            timeoutBalance.add(message.author.id);
+            setTimeout(() => timeoutBalance.delete(message.author.id), 60000);
+        }
+        
+        // If the user pings the bot the bot will respond with it's prefix
+        const mentionRegex = RegExp(`^<@!${client.user.id}>$`);
+        const mentionRegexPrefix = RegExp(`^<@!${client.user.id}> `);
+        if (message.content.match(mentionRegex)) {
+            return message.channel.send(`Heya ${message.author}! My Prefix is \`${GuildConfig.prefix}\``);
+        }
+        
+        var prefix = message.content.match(mentionRegexPrefix) ? message.content.match(mentionRegexPrefix)[0] : GuildConfig.prefix;
+        // Is a command by a guild member who is not a bot? If so execute it
+        if (message.content.startsWith(prefix)) {
+            if (message.member && message.author && !message.author.bot) {
+                commandParts = message.content.slice(prefix.length).trim().split(/ +/g);
+                const command = commandParts.shift().toLowerCase();
+                const cmd = client.commands.get(command) || client.commands.get(client.aliases.get(command));
+                if (!cmd) {
+                    return console.log(`Discord: Command Invalid: ${command} by ${message.author.tag}`)
+                } else {
+                    try {
+                        await cmd.run(client, message, commandParts);
+                    } catch (e) {
                         try {
                             const errorMessage = new MessageEmbed()
                             .setTitle("❌ An Error has Occured!")
@@ -116,5 +115,5 @@ module.exports = async (client, message) => {
                     console.log(`Discord: Command Executed: ${command} by ${message.author.tag}`)
                     
                 }
-            }
-
+            }   
+        }
