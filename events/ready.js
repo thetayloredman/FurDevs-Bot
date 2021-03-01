d125013d8cb208dd648188ae0ef9d18395c7dc01
ilikestohack <cewilhoit@gmail.com>
2021-03-01 17:52:12 -0500

    Fix API not having the modules to run it

--- events/ready.js
+++ events/ready.js
@@ -46,5 +46,6 @@ module.exports = async (client) => {
 
   twoHours.start();
   console.log(`${client.fdevsLog} Bump Reminders Started!`);
+  require("../api.js").run(client)
   console.log(`${client.fdevsLog} ${client.user.username} is Ready!`);
 };
